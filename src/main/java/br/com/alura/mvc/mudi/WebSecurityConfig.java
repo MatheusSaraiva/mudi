19f312ea37fe62bdb71daa1f3d1386046a0df6ac
Matheus Saraiva <saraivarego@gmail.com>
2021-11-27 12:54:16 -0300

    Implementando o Logout

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,8 +23,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 	        .formLogin(form -> form
 	                .loginPage("/login")
+	                .defaultSuccessUrl("/home", true)
 	                .permitAll()
-	            );
+	            )
+	        	.logout(logout -> logout.logoutUrl("/logout"));
 	}
 	
 	@Bean
